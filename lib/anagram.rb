8745bfe73a82e5da1ed1e20c32faf30552cbf21c
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-23 15:42:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,13 @@
-lass Anagram
-  attr_accessor :diaper, :ba, :listen, :allergy
-  
-end
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
